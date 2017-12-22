0b624422ec0f8cb8c72542e7d5741c2aa64504ac
Chandler <ysiwgtus@gmail.com>
2017-12-22 22:49:04 +0800

    把webbbs项目加入仓库

---
+++ models/topic.py
@@ -0,0 +1,31 @@
+import time
+from models import Model
+
+
+class Topic(Model):
+    @classmethod
+    def get(cls, id):
+        m = cls.find_by(id=id)
+        m.views += 1
+        m.save()
+        return m
+
+    def __init__(self, form):
+        self.id = None
+        self.views = 0
+        self.title = form.get('title', '')
+        self.content = form.get('content', '')
+        self.ct = int(time.time())
+        self.ut = self.ct
+        self.user_id = form.get('user_id', '')
+        self.board_id = int(form.get('board_id', -1))
+
+    def replies(self):
+        from .reply import Reply
+        ms = Reply.find_all(topic_id=self.id)
+        return ms
+
+    def board(self):
+        from .board import Board
+        m = Board.find(self.board_id)
+        return m
